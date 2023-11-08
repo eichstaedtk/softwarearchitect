56cd15cc5e8f701984b7706273b8bc21f0857044
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2023-11-08 21:43:21 +0100

    Add REST Project Controller

---
+++ src/main/java/de/eichstaedt/softwarearchitect/projects/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * Created by konrad.eichstaedt@gmx.de on 08.11.23.
+ *
+ * Use Case Software Architect is creating a project
+ */
+package de.eichstaedt.softwarearchitect.projects;
---
+++ src/main/java/de/eichstaedt/softwarearchitect/ui/ProjectResource.java
@@ -0,0 +1,18 @@
+package de.eichstaedt.softwarearchitect.ui;
+
+import de.eichstaedt.softwarearchitect.projects.Project;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.core.MediaType;
+import java.util.List;
+
+@Path("/projects")
+public class ProjectResource {
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public List<Project> projects() {
+        return List.of(new Project.Builder().withName("Software Engineering Batchelor Course").build());
+    }
+}
