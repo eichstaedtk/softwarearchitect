6df3e1026bdc06e6ee28cc31fc7df05d84b0c92e
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2023-11-08 17:22:50 +0100

    Refactoring to my package

--- src/main/java/de/eichstaedt/softwarearchitect/GreetingResource.java
+++ src/main/java/de/eichstaedt/softwarearchitect/GreetingResource.java
@@ -1,4 +1,4 @@
-package org.acme;
+package de.eichstaedt.softwarearchitect;
 
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
--- src/test/java/de/eichstaedt/softwarearchitect/GreetingResourceTest.java
+++ src/test/java/de/eichstaedt/softwarearchitect/GreetingResourceTest.java
@@ -1,4 +1,4 @@
-package org.acme;
+package de.eichstaedt.softwarearchitect;
 
 import io.quarkus.test.junit.QuarkusTest;
 import org.junit.jupiter.api.Test;
