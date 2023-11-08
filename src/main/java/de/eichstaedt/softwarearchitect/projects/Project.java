7269349a1bde1935985f8412013f7eb9da4858f1
Konrad Eichstaedt <konrad.eichstaedt@gmx.de>
2023-11-08 20:50:00 +0100

    Add Builder to project

--- src/main/java/de/eichstaedt/softwarearchitect/projects/Project.java
+++ src/main/java/de/eichstaedt/softwarearchitect/projects/Project.java
@@ -1,5 +1,7 @@
 package de.eichstaedt.softwarearchitect.projects;
 
+import java.util.UUID;
+
 /**
  * Created by konrad.eichstaedt@gmx.de on 08.11.23.
  */
@@ -11,5 +13,35 @@ public class Project {
 
   private String description;
 
+  public Project(Builder builder) {
+    this.id = builder.id;
+    this.name = builder.name;
+    this.description = builder.description;
+  }
+
+  public String getId() {
+    return this.id;
+  }
+
+  public String getName() {
+    return this.name;
+  }
+
+  public static class Builder {
+    private String id = UUID.randomUUID().toString();
+
+    private String name;
+
+    private String description;
+
+    public Builder withName(String name) {
+      this.name = name;
+      return this;
+    }
+
+    public Project build(){
+      return new Project(this);
+    }
+  }
 
 }
--- src/test/java/de/eichstaedt/softwarearchitect/projects/ProjectTest.java
+++ src/test/java/de/eichstaedt/softwarearchitect/projects/ProjectTest.java
@@ -1,5 +1,6 @@
 package de.eichstaedt.softwarearchitect.projects;
 
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Assertions;
@@ -13,8 +14,13 @@ public class ProjectTest {
   @Test
   void testCreateAProject() {
 
-    Project sonar = new Project();
+    Project sonar = new Project.Builder().withName("Sonar").build();
+    Project ereignisdb = new Project.Builder().withName("Ereignisdatenbank").build();
 
     assertNotNull(sonar);
+    assertNotNull(sonar.getId());
+    assertNotNull(sonar.getName());
+
+    assertNotEquals(sonar.getId(),ereignisdb.getId());
   }
 }
